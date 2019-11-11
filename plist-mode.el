09214be98ac0937a8355f71e0e009b1ecd4251ad
Eddie Hillenbrand <eddie@graphdyne.com>
2019-11-10 17:05:43 -0800

    Highlight comments and strings

--- plist-mode.el
+++ plist-mode.el
@@ -2,8 +2,10 @@
 
 ;; Copyright (C) 2019 Eddie Hillenbrand
 
-;; This file is NOT part of GNU Emacs.
+;; Author: YourName
+;; Keywords: extensions
 
+;; This file is NOT part of GNU Emacs.
 ;; This program is actual free software; it uses the permisive MIT
 ;; license NOT the GPL crap license.
 
@@ -15,7 +17,7 @@
 
 ;;; Code:
 
-(require 'cc-mode')
+(require 'cc-mode)
 
 (defgroup plist nil
   "Major mode for editing Old-Style NeXT/OpenStep Property List files."
@@ -23,32 +25,35 @@
   :group 'plist
   :link '(url-link "https://github.com/eddieh/plist-mode"))
 
+(defvar plist-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?{ "(}" st)
+    (modify-syntax-entry ?} "){" st)
+    (modify-syntax-entry ?/ ".  14" st)
+    (modify-syntax-entry ?* ".  23" st)
+    st)
+  "Syntax table for `plist-mode'.")
+
+(defvar plist-mode-font-lock-defaults
+  `((
+     ("\"\\.\\*\\?" . font-lock-string-face))))
+
 ;;;###autoload
 (define-derived-mode plist-mode prog-mode "Plist"
-  "Major mode for editing Caddy configuration files."
+  "Major mode for editing Old-Style NeXT/OpenStep Property List files."
 
-  (setq-local comment-start "/\\*")
+  :syntax-table plist-mode-syntax-table
+
+  (setq-local comment-start "/*")
   (setq-local comment-start-skip "\\(//+\\|/\\*+\\)\\s *")
-  (setq-local comment-end "\\*/")
-  (setq-local comment-column 0)
-  ;; (setq-local comment-use-syntax t)
-
-  ;; for filling, pretend we're cc-mode
-  (setq c-comment-prefix-regexp "//+\\|\\**"
-        c-paragraph-start "\\(@[[:alpha:]]+\\>\\|$\\)"
-        c-paragraph-separate "$"
-        c-block-comment-prefix "* "
-        c-line-comment-starter "//"
-        c-comment-start-regexp "/[*/]\\|\\s!"
-        comment-start-skip "\\(//+\\|/\\*+\\)\\s *")
-  (setq-local comment-line-break-function #'c-indent-new-comment-line)
-  (setq-local c-block-comment-start-regexp "/\\*")
+  (setq-local comment-end "*/")
+  (setq-local comment-use-syntax t)
   (setq-local comment-multi-line t)
 
-  ;; (setq-local font-lock-defaults '(plist-mode-font-lock-keywords))
+  (setq-local font-lock-defaults '(plist-mode-font-lock-defaults))
 
-  (setq-local tab-width 8)
-  (setq-local indent-tabs-mode t)
+  (setq-local tab-width 4)
+  (setq-local indent-tabs-mode nil)
   ;; (setq-local indent-line-function 'plist--indent-line)
   )
 
